56311d88a4e6b12d02047905a7e39374faea2df7
Kunal7008 <kunal.bhatta513@gmail.com>
2020-11-24 20:13:08 +0530

    new class

---
+++ src/com/data/value/Data.java
@@ -0,0 +1,22 @@
+package com.data.value;
+
+import org.testng.annotations.Test;
+
+public class Data {
+
+	@Test
+	public void het() {
+	System.out.println("value");
+	}
+	@Test
+	public void het1() {
+	System.out.println("sex");
+	}
+
+	@Test
+	public void het2() {
+	System.out.println("porn star");
+	}
+
+
+}
