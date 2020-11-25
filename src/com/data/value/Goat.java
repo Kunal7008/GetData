9b331419a83dfe81a3920d1110071401cf58713f
Kunal7008 <kunal.bhatta513@gmail.com>
2020-11-25 20:37:01 +0530

    kunal

--- src/com/data/value/Goat.java
+++ src/com/data/value/Goat.java
@@ -7,4 +7,16 @@ public class Goat {
 public void get() {
 	System.out.println("gat");
 }
+@Test
+public void get1() {
+	System.out.println("gat1");
+}
+@Test
+public void get2() {
+	System.out.println("gat2");
+}
+@Test
+public void get3() {
+	System.out.println("gat3");
+}
 }
